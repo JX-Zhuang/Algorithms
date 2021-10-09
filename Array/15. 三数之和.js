deff61753f9f33e06d1b83f1edf27acc3ef5e273
JX-Zhuang <who.zhuang@gmail.com>
2021-10-09 13:52:03 +0800

    threeSum

--- Array/15. 三数之和.js
+++ Array/15. 三数之和.js
@@ -2,6 +2,26 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
- var threeSum = function(nums) {
-
+var threeSum = function (nums) {
+    const length = nums.length;
+    nums.sort((a, b) => a - b);
+    if (nums[0] > 0 || nums[length - 1] < 0) return [];
+    const result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let l = i + 1, r = length - 1, sum;
+        while (l < r) {
+            sum = nums[i] + nums[l] + nums[r];
+            if (sum === 0) {
+                result.push([nums[i], nums[l], nums[r]]);
+                while (nums[l++] === nums[l]);
+                while (nums[r--] === nums[r]);
+            } else if (sum > 0) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+    }
+    return result;
 };
