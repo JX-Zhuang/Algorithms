6961d1fb769e00baac8c7fc526caadccbbcafefc
JX-Zhuang <who.zhuang@gmail.com>
2021-10-15 23:16:18 +0800

    reverseList

---
+++ LinkedList/206. 反转链表.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    var current = head, next, prev = null;
+    while (current) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+};
