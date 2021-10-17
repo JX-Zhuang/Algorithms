9cd245628913709f27634fbb26093050c20b581e
JX-Zhuang <who.zhuang@gmail.com>
2021-10-17 23:13:53 +0800

    detectCycle

--- LinkedList/142. 环形链表 II.js
+++ LinkedList/142. 环形链表 II.js
@@ -10,6 +10,18 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var detectCycle = function(head) {
-    
+var detectCycle = function (head) {
+    var slow = head, fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            while (head !== slow) {
+                head = head.next;
+                slow = slow.next;
+            }
+            return head;
+        }
+    }
+    return null;
 };
