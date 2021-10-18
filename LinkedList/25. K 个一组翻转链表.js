b8b3263aea3cebe422c7648adc7edc26f44e7cbc
JX-Zhuang <who.zhuang@gmail.com>
2021-10-18 22:51:18 +0800

    reverseKGroup

--- LinkedList/25. K 个一组翻转链表.js
+++ LinkedList/25. K 个一组翻转链表.js
@@ -10,6 +10,35 @@
  * @param {number} k
  * @return {ListNode}
  */
-var reverseKGroup = function(head, k) {
-
+var listSize = function (head) {
+    var size = 0;
+    while (head) {
+        head = head.next;
+        size++;
+    }
+    return size;
+}
+var reverseList = function (head, k) {
+    var oldHead = head, current = head, next, prev = null;
+    while (current && k > 0) {
+        k--;
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    oldHead.next = next;
+    return [prev, oldHead];
+};
+var reverseKGroup = function (head, k) {
+    var size = listSize(head);
+    var newHead = new ListNode(0), prev = newHead;
+    while (size >= k) {
+        size -= k;
+        var [first, last] = reverseList(head, k);
+        prev.next = first;
+        head = last.next;
+        prev = last;
+    }
+    return newHead.next;
 };
