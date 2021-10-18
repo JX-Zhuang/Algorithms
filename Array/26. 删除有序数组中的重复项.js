876c1563fe2e4863ddace7fa041ad5de21f2d2b7
JX-Zhuang <who.zhuang@gmail.com>
2021-10-18 23:09:27 +0800

    removeDuplicates

--- Array/26. 删除有序数组中的重复项.js
+++ Array/26. 删除有序数组中的重复项.js
@@ -2,6 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-
+var removeDuplicates = function (nums) {
+    var lastIndex = 0, i = 0, length = nums.length;
+    while (i < length) {
+        while (i + 1 < length && nums[i] === nums[i + 1]) {
+            i++;
+        }
+        nums[lastIndex] = nums[i];
+    }
+    return lastIndex + 1;
 };
