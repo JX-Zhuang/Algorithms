184f265d35a2e41a329ff2cebefd76452cea9eff
JX-Zhuang <who.zhuang@gmail.com>
2021-10-19 22:07:33 +0800

    merge

--- Array/88. 合并两个有序数组.js
+++ Array/88. 合并两个有序数组.js
@@ -5,6 +5,24 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-
+var merge = function (nums1, m, nums2, n) {
+    var index1 = m - 1, index2 = n - 1, index = m + n - 1;
+    while (index >= 0) {
+        if (index1 < 0) {
+            nums1[index--] = nums2[index2--];
+            continue;
+        }
+        if (index2 < 0) {
+            nums1[index--] = nums1[index1--];
+            continue;
+        }
+        if (nums1[index1] >= nums2[index2]) {
+            nums1[index] = nums1[index1];
+            index1--;
+        } else {
+            nums1[index] = nums2[index2];
+            index2--;
+        }
+        index--;
+    }
 };
