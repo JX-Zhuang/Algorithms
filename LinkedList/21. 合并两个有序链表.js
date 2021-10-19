c209d58d075fda1125596df57a68b7ee2480911d
JX-Zhuang <who.zhuang@gmail.com>
2021-10-19 21:47:26 +0800

    mergeTwoLists

--- LinkedList/21. 合并两个有序链表.js
+++ LinkedList/21. 合并两个有序链表.js
@@ -10,6 +10,19 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    
+var mergeTwoLists = function (l1, l2) {
+    var l = new ListNode();
+    var current = l;
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+    current.next = l1 ? l1 : l2;
+    return l.next;
 };
