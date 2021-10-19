0ca1b9c63f2dbae099b9945153154d58c362b5bb
JX-Zhuang <who.zhuang@gmail.com>
2021-10-19 21:38:18 +0800

    rotate

--- Array/189. 旋转数组.js
+++ Array/189. 旋转数组.js
@@ -3,6 +3,17 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-
+var reverse = function (arr, start, end) {
+    while (start < end) {
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+    }
+}
+var rotate = function (nums, k) {
+    var l = nums.length;
+    k %= l;
+    reverse(nums, 0, l - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, l - 1);
 };
