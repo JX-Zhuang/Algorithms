23ccd280f8951f8690ce9921435394d02fed15d4
JX-Zhuang <who.zhuang@gmail.com>
2021-10-20 22:33:03 +0800

    isValid

--- Stack/20. 有效的括号.js
+++ Stack/20. 有效的括号.js
@@ -2,6 +2,17 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+    var stack = [], last;
+    for (var item of s) {
+        if (item === '(' || item === '{' || item === '[') {
+            stack.push(item);
+            continue;
+        }
+        last = stack.pop();
+        if (item === ')' && last !== '(') return false;
+        if (item === ']' && last !== '[') return false;
+        if (item === '}' && last !== '{') return false;
+    }
+    return stack.length === 0;
 };
