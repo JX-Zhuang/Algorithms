1704933631d3072753025216b7aaa9ff286fe096
JX-Zhuang <who.zhuang@gmail.com>
2021-10-26 22:43:03 +0800

    trap

--- Stack/42. 接雨水.js
+++ Stack/42. 接雨水.js
@@ -2,6 +2,16 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
-
+var trap = function (height) {
+    var result = 0, l = 0, r = height.length - 1, lMax = height[l], rMax = height[r];
+    while (l < r) {
+        lMax = Math.max(height[l], lMax);
+        rMax = Math.max(height[r], rMax);
+        if (lMax < rMax) {
+            result += lMax - height[l++];
+        } else {
+            result += rMax - height[r--];
+        }
+    }
+    return result;
 };
