05d50aaf808388f6b2bf18f4e56e4c98b951c379
JX-Zhuang <who.zhuang@gmail.com>
2021-11-05 21:48:19 +0800

    inorderTraversal

---
+++ Tree/94. 二叉树的中序遍历.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+    var result = [];
+    var inorder = function (root) {
+        if (!root) return;
+        inorder(root.left);
+        result.push(root.val);
+        inorder(root.right);
+    };
+    inorder(root);
+    return result;
+};
