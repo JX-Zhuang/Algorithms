98e0d580e4fa6b6cb75674a58963b6ae656b6a9a
JX-Zhuang <who.zhuang@gmail.com>
2021-11-07 20:06:11 +0800

    generateParenthesis

--- Recursion/22. 括号生成.js
+++ Recursion/22. 括号生成.js
@@ -3,5 +3,15 @@
  * @return {string[]}
  */
 var generateParenthesis = function (n) {
-
+    var result = [];
+    generate(0, 0, n, '', result);
+    return result;
 };
+var generate = function (left, right, n, str, result) {
+    if (left === n && right === n) {
+        result.push(str);
+        return result;
+    }
+    if (left < n) generate(left + 1, right, n, str + '(', result);
+    if (left > right) generate(left, right + 1, n, str + ')', result);
+}
