1d895d071deddd7f2cc707da6cc6ca9815e23ffd
JX-Zhuang <who.zhuang@gmail.com>
2021-11-10 12:52:53 +0800

    invertTree

---
+++ Recursion/226. 翻转二叉树.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (!root) return null;
+    var right = root.right, left = root.left;
+    root.right = invertTree(left);
+    root.left = invertTree(right);
+    return root;
+};
