ad79af771cbdbdc637319b6fc5998878a2a1ccfd
JX-Zhuang <who.zhuang@gmail.com>
2021-11-29 14:06:35 +0800

    buildTree

--- Recursion/105. 从前序与中序遍历序列构造二叉树.js
+++ Recursion/105. 从前序与中序遍历序列构造二叉树.js
@@ -11,6 +11,19 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-var buildTree = function(preorder, inorder) {
-
+var buildTree = function (preorder, inorder) {
+    var l = preorder.length;
+    var helper = function (preStart, preEnd, inStart, inEnd) {
+        if (preEnd < preStart || inEnd < inStart) return null;
+        if (preStart < 0 || preEnd >= l || inStart < 0 || inEnd >= l) return null;
+        var rootVal = preorder[preStart];
+        var rootValIndexOfInorder = inorder.indexOf(rootVal);
+        var nextPreEnd = preStart + (rootValIndexOfInorder - inStart);
+        var nextPreStart = preEnd - (inEnd - rootValIndexOfInorder) + 1;
+        var root = new TreeNode(rootVal);
+        root.left = helper(preStart + 1, nextPreEnd, inStart, rootValIndexOfInorder - 1);
+        root.right = helper(nextPreStart, preEnd, rootValIndexOfInorder + 1, inEnd);
+        return root;
+    };
+    return helper(0, l - 1, 0, l - 1);
 };
