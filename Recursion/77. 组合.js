1ca56799dd87e24d6999a8cf41f116d494a42045
JX-Zhuang <who.zhuang@gmail.com>
2021-12-01 22:38:15 +0800

    combine

--- Recursion/77. 组合.js
+++ Recursion/77. 组合.js
@@ -4,5 +4,18 @@
  * @return {number[][]}
  */
 var combine = function (n, k) {
-
+    var result = [];
+    var helper = function (start, arr) {
+        if (arr.length === k) {
+            result.push(arr.slice());
+            return;
+        }
+        for (var i = start; i <= n; i++) {
+            arr.push(i);
+            helper(i + 1, arr);
+            arr.pop();
+        }
+    }
+    helper(1, []);
+    return result;
 };
