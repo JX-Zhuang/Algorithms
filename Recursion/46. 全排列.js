7ec7425a5259ba2e1ad98ab91c4da8442b6dd60a
JX-Zhuang <who.zhuang@gmail.com>
2021-12-02 23:40:36 +0800

    permute

--- Recursion/46. 全排列.js
+++ Recursion/46. 全排列.js
@@ -2,6 +2,18 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
-
+var permute = function (nums) {
+    var result = [];
+    var helper = function (arr) {
+        if (arr.length === nums.length) {
+            result.push(arr);
+            return;
+        }
+        for (var i of nums) {
+            if (arr.includes(i)) continue;
+            helper(arr.concat(i));
+        }
+    };
+    helper([]);
+    return result;
 };
