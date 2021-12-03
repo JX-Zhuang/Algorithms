dbc15281b7cde513b318e24aa8662462041ea699
JX-Zhuang <who.zhuang@gmail.com>
2021-12-04 00:34:52 +0800

    permuteUnique

--- Recursion/47. 全排列 II.js
+++ Recursion/47. 全排列 II.js
@@ -2,6 +2,24 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permuteUnique = function(nums) {
-
+var permuteUnique = function (nums) {
+    var result = [];
+    var visited = [];
+    var backtrack = function (index, arr) {
+        if (arr.length === nums.length) {
+            result.push([...arr]);
+            return;
+        }
+        for (var i in nums) {
+            if (visited[i] || (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])) continue;
+            visited[i] = true;
+            arr.push(nums[i]);
+            backtrack(index + 1, arr);
+            arr.pop();
+            visited[i] = false;
+        }
+    };
+    nums.sort((a, b) => a - b);
+    backtrack(0, []);
+    return result;
 };
