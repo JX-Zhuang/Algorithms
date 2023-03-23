fd9f66592ba60470de34b5373bdcc4e41c2c8e19
JX-Zhuang <who.zhuang@gmail.com>
2023-03-23 19:17:42 +0800

    update

--- Graph/cycle/type.ts
+++ Graph/cycle/type.ts
@@ -2,25 +2,25 @@ export interface Edge {
     id: string;
     source: {
         cell: string;
-        [x:string]:any;
+        // [x:string]:any;
     };
     target: {
         cell: string;
-        [x:string]:any;
+        // [x:string]:any;
     };
     data: {
         type: 'EDGE',
-        [x:string]:any;
+        // [x:string]:any;
     }
-    [x:string]:any;
+    // [x:string]:any;
 };
 export interface Node {
     id: string;
     data: {
         type: 'NODE' | 'START';
         name: string;
-        [x:string]:any;
+        // [x:string]:any;
     };
-    [x:string]:any;
+    // [x:string]:any;
 };
 export type Data = Node | Edge;
